2f9d1e446f5a8314474aaf298961b12c4307b677
Fabien Pinckaers <fp@openerp.com>
2015-08-10 19:36:06 +0200

    [IMP] Timesheets

--- addons/hr_timesheet/hr_timesheet.py
+++ addons/hr_timesheet/hr_timesheet.py
@@ -8,10 +8,23 @@ class Company(models.Model):
     _inherit = 'res.company'
     timesheet_uom_id = fields.Many2one('product.uom', 'Timesheet UoM')
 
+class product_template(models.Model):
+    _inherit = 'product.template'
+    is_timesheet = fields.Boolean(string="Track Service Time")
+
+    @api.onchange('type', 'invoice_policy')
+    def onchange_type(self):
+        if self.type=='service' and self.invoice_policy=='time material':
+            self.is_timesheet = True
+        if self.type<>'service':
+            self.is_timesheet = False
+        return {}
+
 class account_analytic_line(models.Model):
     _inherit = 'account.analytic.line'
-    is_timesheet = fields.Boolean()
+    is_timesheet = fields.Boolean(string="Is a Timesheet")
 
 class account_analytic_account(models.Model):
     _inherit = 'account.analytic.account'
     use_timesheets = fields.Boolean('Timesheets', help="Check this field if this project manages timesheets", deprecated=True)
+
--- addons/sale_timesheet/models/sale_timesheet.py
+++ addons/sale_timesheet/models/sale_timesheet.py
@@ -25,14 +25,34 @@ class AccountAnalyticLine(models.Model):
         sol_obj = self.env['sale.order.line']
         for line in self:
             if line.is_timesheet:
-                sol = sol_obj.search([
-                    ('order_id.project_id','=',result.account_id.id),
-                    ('state','=','sale'),
-                    ('product_id.invoice_policy','=','time material'),
-                    ('product_id.type','=','service')])
+                if not line.so_line:
+                    sol = sol_obj.search([
+                        ('order_id.project_id','=',result.account_id.id),
+                        ('state','=','sale'),
+                        ('product_id.is_timesheet','=',True),
+                        ('product_id.type','=','service'])
+                    if sol:
+                        line.so_line = sol[0]
+                        sol = sol[0]
+                else:
+                    sol = line.so_line
                 if sol:
-                    line.product_id = sol[0].product_id.id
-                    line.uom_id = self.env.user.company_id.timesheet_uom_id.id or sol[0].product_uom.id
+                    line.product_id = sol.product_id.id
+                    line.uom_id = self.env.user.company_id.timesheet_uom_id.id or sol.product_uom.id
+                    line.amount = -line.unit_amount * sol.product_id.standard_price
         return True
 
+class SaleOrder(models.Model):
+    _inherit = 'sale.order'
+
+    @api.one
+    @api.constrains('order_line.product_id')
+    def _check_children_scope(self):
+        count = 0
+        for line in self.order_line:
+            if line.product_id.is_timesheet:
+                count+=1
+            if count > 1:
+                raise UserError(_("You can use only one product on timesheet within the same sale order. You should split your order to include only one contract based on time and material."))
+        return {}
 
